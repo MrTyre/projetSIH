8f0db13fd518b3f8f3ab03aa46cd39932d8dddda
dormarina <dor.marina92@gmail.com>
2015-02-26 09:38:55 +0100

    few modifs (of course c'est pas Marina..)

--- src/projet/UI/LettreSortie.java
+++ src/projet/UI/LettreSortie.java
@@ -8,8 +8,13 @@ package projet.UI;
 import projet.sih.*;
 
 import java.io.File;
+import java.sql.Date;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JOptionPane;
+import org.odftoolkit.odfdom.doc.OdfTextDocument;
 
 /**
  *
@@ -17,11 +22,16 @@ import java.text.SimpleDateFormat;
  */
 public class LettreSortie extends javax.swing.JFrame {
 
+    private Patient currentPatient;
+    private PH currentPH;
+
     /**
      * Creates new form LettreSortie
      */
     public LettreSortie() {
         initComponents();
+        setDefaultCloseOperation(DISPOSE_ON_CLOSE);
+        setLocationRelativeTo(null);
     }
 
     /**
@@ -43,10 +53,10 @@ public class LettreSortie extends javax.swing.JFrame {
         jLabelMedecin = new javax.swing.JLabel();
         jLabelDiagnostic = new javax.swing.JLabel();
         jScrollPane1 = new javax.swing.JScrollPane();
-        jTextArea1 = new javax.swing.JTextArea();
+        jTextAreaDiagnostic = new javax.swing.JTextArea();
         jLabelTraitement = new javax.swing.JLabel();
         jScrollPane2 = new javax.swing.JScrollPane();
-        jTextArea2 = new javax.swing.JTextArea();
+        jTextAreaTraitement = new javax.swing.JTextArea();
         jButtonOK = new javax.swing.JButton();
         jButtonAnnuler = new javax.swing.JButton();
 
@@ -74,28 +84,38 @@ public class LettreSortie extends javax.swing.JFrame {
         jLabelDiagnostic.setFont(new java.awt.Font("Tahoma", 0, 12)); // NOI18N
         jLabelDiagnostic.setText("Diagnostic :");
 
-        jTextArea1.setColumns(20);
-        jTextArea1.setFont(new java.awt.Font("Tahoma", 0, 12)); // NOI18N
-        jTextArea1.setLineWrap(true);
-        jTextArea1.setRows(5);
-        jScrollPane1.setViewportView(jTextArea1);
+        jTextAreaDiagnostic.setColumns(20);
+        jTextAreaDiagnostic.setFont(new java.awt.Font("Tahoma", 0, 12)); // NOI18N
+        jTextAreaDiagnostic.setLineWrap(true);
+        jTextAreaDiagnostic.setRows(5);
+        jScrollPane1.setViewportView(jTextAreaDiagnostic);
 
         jLabelTraitement.setFont(new java.awt.Font("Tahoma", 0, 12)); // NOI18N
         jLabelTraitement.setText("Traitement réalisé : ");
 
-        jTextArea2.setColumns(20);
-        jTextArea2.setFont(new java.awt.Font("Tahoma", 0, 12)); // NOI18N
-        jTextArea2.setLineWrap(true);
-        jTextArea2.setRows(5);
-        jScrollPane2.setViewportView(jTextArea2);
+        jTextAreaTraitement.setColumns(20);
+        jTextAreaTraitement.setFont(new java.awt.Font("Tahoma", 0, 12)); // NOI18N
+        jTextAreaTraitement.setLineWrap(true);
+        jTextAreaTraitement.setRows(5);
+        jScrollPane2.setViewportView(jTextAreaTraitement);
 
         jButtonOK.setBackground(new java.awt.Color(0, 51, 153));
         jButtonOK.setFont(new java.awt.Font("Tahoma", 0, 12)); // NOI18N
         jButtonOK.setText("OK");
+        jButtonOK.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButtonOKActionPerformed(evt);
+            }
+        });
 
         jButtonAnnuler.setBackground(new java.awt.Color(0, 51, 153));
         jButtonAnnuler.setFont(new java.awt.Font("Tahoma", 0, 12)); // NOI18N
         jButtonAnnuler.setText("Annuler");
+        jButtonAnnuler.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButtonAnnulerActionPerformed(evt);
+            }
+        });
 
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
         jPanel1.setLayout(jPanel1Layout);
@@ -176,6 +196,23 @@ public class LettreSortie extends javax.swing.JFrame {
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
+    private void jButtonAnnulerActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButtonAnnulerActionPerformed
+        setVisible(false);
+    }//GEN-LAST:event_jButtonAnnulerActionPerformed
+
+    private void jButtonOKActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButtonOKActionPerformed
+        JOptionPane j = new JOptionPane();
+        int retour = j.showConfirmDialog(this, "Êtes-vous sûr de vouloir créer cette lettre ?", "Confirmation", JOptionPane.OK_CANCEL_OPTION);
+        if (retour == JOptionPane.OK_OPTION) {
+            try {
+                GenererLettreSortie(currentPH, currentPatient);
+            } catch (Exception ex) {
+                Logger.getLogger(LettreSortie.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+
+    }//GEN-LAST:event_jButtonOKActionPerformed
+
     /**
      * @param args the command line arguments
      */
@@ -226,57 +263,57 @@ public class LettreSortie extends javax.swing.JFrame {
     private javax.swing.JPanel jPanel1;
     private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.JScrollPane jScrollPane2;
-    private javax.swing.JTextArea jTextArea1;
-    private javax.swing.JTextArea jTextArea2;
+    private javax.swing.JTextArea jTextAreaDiagnostic;
+    private javax.swing.JTextArea jTextAreaTraitement;
     // End of variables declaration//GEN-END:variables
-    
-//    static public void GenererLettreSortie(PH ph1,Patient p1,Date d) throws Exception{
-//        DateFormat df1 = new SimpleDateFormat("dd-MM-yyyy");
-//        DateFormat df2 = new SimpleDateFormat("hh:mm");
-//        // Create a text document from a standard template (empty documents within the JAR)
-//        OdfTextDocument odt = OdfTextDocument.newTextDocument();
-//
-//        // Append text to the end of the document. 
-//        odt.newImage(new File("C:/Users/Tommy/Documents/GitHub/Images/CHUPP.jpg").toURI());
-//        odt.newParagraph();
-//        odt.addText("Dr."+ph1.getNom()+" "+ph1.getPrenom());
-//        odt.newParagraph();
-//        odt.addText("CHU de Princeton-Plainsboro");
-//        odt.newParagraph();
-//        odt.addText("Tel : XX.XX.XX.XX.XX");
-//        odt.newParagraph();
-//        odt.newParagraph();
-//        odt.newParagraph();
-//        odt.newParagraph();
-//        odt.addText("__________________________________________________________________ le "+df1.format(d)+",");
-//        odt.newParagraph();
-//        odt.newParagraph();
-//        odt.newParagraph();
-//        odt.addText("\n\nObjet : Sortie de Mr."+p1.getNom()+" "+p1.getPrenom());
-//        odt.newParagraph();        
-//        odt.newParagraph();
-//        odt.addText("\n\nJe soussigné Dr."+ph1.getNom()+" "+ph1.getPrenom()+", autorise le patient "+p1.getNom()+" "+p1.getPrenom()+" a sortir du Service "+ph1.getSpecialite()+" à compter du "+df1.format(d)+" à "+df2.format(d)+"h.");
-//        odt.newParagraph();
-//        odt.addText("Cette sortie intervient suite au diagnostic suivant :");
-//        odt.newParagraph();
-//        //odt.addText(jTextFieldDiagnostic.getText());
-//        odt.newParagraph();
-//        odt.addText("Le patient "+p1.getNom()+" "+p1.getPrenom()+" est autorisé à sortir du Service "+ph1.getSpecialite()+" suite à l'administration du traitement suivant :");
-//        odt.newParagraph();
-//        //odt.addText(jTextFieldTraitement.getText());
-//        odt.newParagraph();
-//        odt.newParagraph();
-//        odt.newParagraph();
-//        odt.newParagraph();
-//        odt.addText("Cordialement,");
-//        odt.newParagraph();
-//        odt.newParagraph();
-//        odt.addText("Dr "+ph1.getNom()+" "+ph1.getPrenom());
-//        odt.newParagraph();
-//        //odt.addText("Signature :"+jTextFieldSignature.getText());       
-//
-//        // Save document
-//        odt.save("C:/Users/Tommy/Documents/GitHub/Projet SIH - Netbeans/Lettres/Lettre de sortie de Mr " + p1.getNom() +" " + p1.getPrenom() +", né le "+df1.format(p1.getDateNaissance())+".odt");
-//
-//    }
+
+    static public void GenererLettreSortie(PH ph1, Patient p1) throws Exception {
+        Date d = new Date(new java.util.Date().getTime());
+        DateFormat df1 = new SimpleDateFormat("dd-MM-yyyy");
+        DateFormat df2 = new SimpleDateFormat("hh:mm");
+        // Create a text document from a standard template (empty documents within the JAR)
+        OdfTextDocument odt = OdfTextDocument.newTextDocument();
+
+        // Append text to the end of the document. 
+        odt.newImage(new File("C:/Users/Marina/Documents/GitHub/projetSIH/projetSIH/src/projet/UI/Princeton-Plainsboro.jpg").toURI());
+        odt.newParagraph();
+        odt.addText("Dr." + ph1.getNom() + " " + ph1.getPrenom());
+        odt.newParagraph();
+        odt.addText("CHU de Princeton-Plainsboro");
+        odt.newParagraph();
+        odt.addText("Tel : XX.XX.XX.XX.XX");
+        odt.newParagraph();
+        odt.newParagraph();
+        odt.newParagraph();
+        odt.newParagraph();
+        odt.addText("__________________________________________________________________ le " + df1.format(d) + ",");
+        odt.newParagraph();
+        odt.newParagraph();
+        odt.newParagraph();
+        odt.addText("\n\nObjet : Sortie de Mr." + p1.getNom() + " " + p1.getPrenom());
+        odt.newParagraph();
+        odt.newParagraph();
+        odt.addText("\n\nJe soussigné Dr." + ph1.getNom() + " " + ph1.getPrenom() + ", autorise le patient " + p1.getNom() + " " + p1.getPrenom() + " a sortir du Service " + ph1.getSpecialite() + " à compter du " + df1.format(d) + " à " + df2.format(d) + "h.");
+        odt.newParagraph();
+        odt.addText("Cette sortie intervient suite au diagnostic suivant :");
+        odt.newParagraph();
+        //odt.addText(jTextAreaDiagnostic.getText());
+        odt.newParagraph();
+        odt.addText("Le patient " + p1.getNom() + " " + p1.getPrenom() + " est autorisé à sortir du Service " + ph1.getSpecialite() + " suite à l'administration du traitement suivant :");
+        odt.newParagraph();
+        //odt.addText(jTextAreaTraitement.getText());
+        odt.newParagraph();
+        odt.newParagraph();
+        odt.newParagraph();
+        odt.newParagraph();
+        odt.addText("Cordialement,");
+        odt.newParagraph();
+        odt.newParagraph();
+        odt.addText("Dr " + ph1.getNom() + " " + ph1.getPrenom());
+        odt.newParagraph();
+
+        // Save document
+        odt.save("C:/Users/Marina/Documents/GitHub/Lettre de sortie de Mr " + p1.getNom() + " " + p1.getPrenom() + ", né le " + df1.format(p1.getDateNaissance()) + ".odt");
+
+    }
 }
